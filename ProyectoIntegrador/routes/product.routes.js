38aaf0efadc63ea446663fa84f1fa3ce927d77da
Juan Miguel Garcia Habeych <86024123+JMGHabeycH@users.noreply.github.com>
2023-02-15 13:11:00 -0500

    Delete ProyectoIntegrador directory

--- ProyectoIntegrador/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const Article = require("../models/product");
-const router = express.Router();
-const cloudinary = require("../utils/cloudinary");
-const upload = require("../utils/multer");
-
-
-
-module.exports = router;
